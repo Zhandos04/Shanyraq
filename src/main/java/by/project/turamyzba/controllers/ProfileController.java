196396967f260363ab8f2e6ba7fe93f2f7a88a95
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-10-20 12:41:01 +0500

    Added Swagger for ProfileController

--- src/main/java/by/project/turamyzba/controllers/ProfileController.java
+++ src/main/java/by/project/turamyzba/controllers/ProfileController.java
@@ -3,6 +3,9 @@ package by.project.turamyzba.controllers;
 import by.project.turamyzba.dto.ProfileDTO;
 import by.project.turamyzba.services.ProfileService;
 import by.project.turamyzba.util.IncorrectJSONException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -24,10 +27,19 @@ public class ProfileController {
     }
 
     @GetMapping
+    @Operation(summary = "Register a new user", description = "Registers a new user by checking for existing IDs and phone numbers.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved profile")
+    @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource")
+    @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden")
+    @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
     public ProfileDTO get() {
         return profileService.getUser();
     }
     @PutMapping("/edit")
+    @Operation(summary = "Edit user profile", description = "Updates the user profile based on provided data")
+    @ApiResponse(responseCode = "200", description = "Profile updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid input")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public HttpStatus edit(@RequestBody @Valid ProfileDTO profileDTO, BindingResult bindingResult) throws IncorrectJSONException {
         if (bindingResult.hasErrors()) {
             List<FieldError> errors = bindingResult.getFieldErrors();
--- src/main/java/by/project/turamyzba/services/ProfileService.java
+++ src/main/java/by/project/turamyzba/services/ProfileService.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @Transactional(readOnly = true)
 public class ProfileService {
