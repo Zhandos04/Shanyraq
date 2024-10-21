824c8a039ae7c84a7133a4f9e79d2d42f34a9f13
sembekov <76952248+sembekov@users.noreply.github.com>
2024-10-22 01:26:15 +0500

    Add new repositories

---
+++ src/main/java/by/project/turamyzba/repositories/PhoneRepository.java
@@ -0,0 +1,12 @@
+package by.project.turamyzba.repositories;
+
+import by.project.turamyzba.models.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PhoneRepository extends JpaRepository<Phone, Integer> {
+    Optional<Phone> findByPhoneNumber(String phoneNumber);
+}
