1e96dd70a49e7d4bc561bd467b517add5b54c3c1
sembekov <76952248+sembekov@users.noreply.github.com>
2024-10-22 01:25:58 +0500

    Add new models

---
+++ src/main/java/by/project/turamyzba/models/Phone.java
@@ -0,0 +1,31 @@
+package by.project.turamyzba.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.Instant;
+
+@Entity
+@Table(name = "phones")
+@Data
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "is_verify")
+    private Boolean isVerify;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
