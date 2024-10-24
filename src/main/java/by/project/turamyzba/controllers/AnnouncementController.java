29b4df8bf6a90b22abd0d0d79e143d606255b467
Hayzerr <bolashak040206@mail.ru>
2024-10-24 17:35:28 +0500

    Added validators to announcement

--- src/main/java/by/project/turamyzba/controllers/AnnouncementController.java
+++ src/main/java/by/project/turamyzba/controllers/AnnouncementController.java
@@ -4,6 +4,7 @@ import by.project.turamyzba.dto.requests.AnnouncementRequest;
 import by.project.turamyzba.dto.responses.AnnouncementResponse;
 import by.project.turamyzba.models.Announcement;
 import by.project.turamyzba.services.AnnouncementService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +16,13 @@ public class AnnouncementController {
     private AnnouncementService announcementService;
 
     @PostMapping("/create")
-    public ResponseEntity<AnnouncementResponse> createAnnouncement(@RequestBody AnnouncementRequest announcementRequest) {
+    public ResponseEntity<AnnouncementResponse> createAnnouncement(@RequestBody @Valid AnnouncementRequest announcementRequest) {
         AnnouncementResponse announcementResponse = announcementService.createAnnouncement(announcementRequest);
         return ResponseEntity.ok(announcementResponse);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<AnnouncementResponse> updateAnnouncement(@PathVariable Long id, @RequestBody AnnouncementRequest announcementRequest) {
+    public ResponseEntity<AnnouncementResponse> updateAnnouncement(@PathVariable Long id, @RequestBody @Valid AnnouncementRequest announcementRequest) {
         AnnouncementResponse announcementResponse = announcementService.updateAnnouncement(id, announcementRequest);
         return ResponseEntity.ok(announcementResponse);
     }
