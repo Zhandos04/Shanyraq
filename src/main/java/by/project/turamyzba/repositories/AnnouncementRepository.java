f23135b50c63853d00dc102ab77b5eabe9a3a441
smilexah <smilexah@inbox.ru>
2024-10-26 16:23:45 +0500

    added some functionality

--- src/main/java/by/project/turamyzba/repositories/AnnouncementRepository.java
+++ src/main/java/by/project/turamyzba/repositories/AnnouncementRepository.java
@@ -1,14 +1,23 @@
 package by.project.turamyzba.repositories;
 
 import by.project.turamyzba.entities.Announcement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer>, JpaSpecificationExecutor<Announcement> {
     List<Announcement> findByUserId(Long userId);
     Optional<Announcement> findById(Long id);
+
+    Page<Announcement> findByTitleContaining(
+            @Param("title") String title,
+            Pageable pageable
+    );
 }
