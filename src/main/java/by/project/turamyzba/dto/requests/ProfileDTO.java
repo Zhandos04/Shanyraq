d41ff0ceb75f3e86e7f2ddc222770a8fd2925531
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-11-25 20:13:49 +0500

    updated profile

--- src/main/java/by/project/turamyzba/dto/requests/ProfileDTO.java
+++ src/main/java/by/project/turamyzba/dto/requests/ProfileDTO.java
@@ -1,5 +1,7 @@
 package by.project.turamyzba.dto.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +9,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProfileDTO {
-    @Pattern(regexp = "[A-Z]\\w+ [A-Z]\\w+", message = "fullname")
-    private String fullName;
-    private String nickName;
+    @Pattern(regexp = "^[A-Z][a-z]*$", message = "FirstName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой")
+    private String firstName;
+    @Pattern(regexp = "^[A-Z][a-z]*$", message = "LastName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой")
+    private String lastName;
+    @Email(message = "Неверный формат email")
+    private String email;
+    @NotBlank(message = "Birth date cannot be empty")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Invalid date format. Expected yyyy-MM-dd")
+    private String birthDate;
+    private String phoneNumber;
+    private String gender;
 }
