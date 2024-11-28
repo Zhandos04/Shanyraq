8847d8145050c42e74f53d8d77ff73e1dbecc6a8
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-11-29 00:32:03 +0500

    search and sort

--- src/main/java/by/project/turamyzba/services/AnnouncementService.java
+++ src/main/java/by/project/turamyzba/services/AnnouncementService.java
@@ -15,23 +15,14 @@ import java.util.List;
 public interface AnnouncementService {
     void createAnnouncement(AnnouncementRequest announcementRequest) throws IOException;
     Page<Announcement> getAllRoommateListings(Pageable pageable);
-
-    Page<Announcement> searchRoommateListings(String search, Pageable pageable);
-
+    Page<Announcement> searchRoommateListings(String region, Integer minPrice, Integer maxPrice, String gender, Integer roommatesCount, Pageable pageable);
     AnnouncementResponse getAnnouncementById(Long id);
-
     List<AnnouncementResponse> getUserAnnouncements();
-
     List<AnnouncementResponse> getUserArchiveAnnouncements();
-
     void archiveAnnouncement(Long announcementId) throws BadRequestException;
-
     AnnouncementResponse updateAnnouncement(Long id, AnnouncementRequest announcementRequest);
-
     List<Announcement> getFilteredAnnouncements(AnnouncementFilterRequest request);
-
     void restoreAnnouncement(Long announcementId) throws BadRequestException;
     void deleteAnnouncement(Long announcementId) throws BadRequestException;
-
     AnnouncementResponse toAnnouncementResponse(Announcement announcement);
 }
