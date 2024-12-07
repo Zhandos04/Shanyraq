125cd40efbca8820fd8017eb745213891af8b027
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-12-07 23:25:35 +0500

    updated validation for profile dto

--- src/main/java/by/project/turamyzba/dto/requests/ProfileDTO.java
+++ src/main/java/by/project/turamyzba/dto/requests/ProfileDTO.java
@@ -1,7 +1,5 @@
 package by.project.turamyzba.dto.requests;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +7,33 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProfileDTO {
-    @Pattern(regexp = "^[A-Z][a-z]*$", message = "FirstName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой")
+    @Pattern(
+            regexp = "^$|^[A-Z][a-z]*$",
+            message = "FirstName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой"
+    )
     private String firstName;
-    @Pattern(regexp = "^[A-Z][a-z]*$", message = "LastName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой")
+
+    @Pattern(
+            regexp = "^$|^[A-Z][a-z]*$",
+            message = "LastName должно начинаться с заглавной буквы и содержать только маленькие буквы после первой"
+    )
     private String lastName;
-    @NotBlank(message = "Birth date cannot be empty")
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Invalid date format. Expected yyyy-MM-dd")
+
+    @Pattern(
+            regexp = "^$|\\d{4}-\\d{2}-\\d{2}",
+            message = "Invalid date format. Expected yyyy-MM-dd"
+    )
     private String birthDate;
-    @Pattern(regexp = "^((\\+7)\\d{10})$",
-            message = "phoneNumber")
+
+    @Pattern(
+            regexp = "^$|^(\\+7)\\d{10}$",
+            message = "Phone number должен соответствовать формату +7XXXXXXXXXX"
+    )
     private String phoneNumber;
+
+    @Pattern(
+            regexp = "^$|^(Мужчина|Женщина|Любой)$",
+            message = "Gender должно быть 'Мужчина', 'Женщина' или 'Любой'"
+    )
     private String gender;
 }
